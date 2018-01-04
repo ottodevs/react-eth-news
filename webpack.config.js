e951c984d555dab76ad5f733834260f5a14cb836
yungtiec <tctammychu@gmail.com>
2018-01-04 15:07:25 +0800

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,55 @@
 var webpack = require('webpack');
+const LiveReloadPlugin = require('webpack-livereload-plugin')
+const isDev = process.env.NODE_ENV === 'development'
+
 module.exports = {
-    entry: ['babel-polyfill', './src/index.js'],
-    output: {
-        path: __dirname,
-        filename: './public/bundle.js'
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [{
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.svg$|\.ttf?|\.woff$|\.woff2|\.eof|\.eot/,
-                loader: 'file-loader'
-            }
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [{
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
         ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-            "window.jQuery": "jquery",
-            "Tether": 'tether',
-        })
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.svg$|\.ttf?|\.woff$|\.woff2|\.eof|\.eot/,
+        loader: 'file-loader'
+      }
     ]
+  },
+  plugins: isDev ? [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      "Tether": 'tether',
+    }),
+    new LiveReloadPlugin({ appendScriptTag: true })
+  ] : [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      "Tether": 'tether',
+    })
+  ]
 }
